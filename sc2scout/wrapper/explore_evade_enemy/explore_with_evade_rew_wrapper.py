8f22c732e270a53d76925ba5d337332a8a23c168
scutzck033 <404762007@qq.com>
2018-08-24 18:20:33 +0800

    Update explore_with_evade_rew_wrapper.py

--- sc2scout/wrapper/explore_evade_enemy/explore_with_evade_rew_wrapper.py
+++ sc2scout/wrapper/explore_evade_enemy/explore_with_evade_rew_wrapper.py
@@ -7,11 +7,11 @@ class ExploreWithEvadeRwdWrapper(ScoutExploreEvadeRwd):
         super(ExploreWithEvadeRwdWrapper, self).__init__(env)
 
     def _assemble_reward(self):
-        self._forward_rewards = [sr.HomeReward(negative=False,weight=0.05),
-                                 sr.EnemyBaseReward(negative=False,weight=0.05),
+        self._forward_rewards = [sr.HomeReward(negative=True,weight=0.1),
+                                 sr.EnemyBaseReward(negative=True,weight=0.1),
                                  sr.ViewEnemyReward(weight=0.5),
                                  # sr.EnemyBaseArrivedReward(weight=50),
-                                 sr.MinDistReward(negative=True,weight=0.05),
+                                 sr.MinDistReward(negative=True,weight=0.1),
                                  # er.EvadeDistanceReward(weight=1),
                                  
                                  # er.EnemyInRangeRwd(weight=1),
@@ -24,12 +24,12 @@ class ExploreWithEvadeRwdWrapper(ScoutExploreEvadeRwd):
                                  sr.ExploreStateRwd(weight=1),
                                  # er.EvadeDistanceReward(weight=1),
                                  
-                                 sr.ViewEnemyResourcesAndBase(weight=0.5),
+                                 sr.ViewEnemyResourcesAndBase(weight=1),
                                  # er.EnemyInRangeRwd(weight=1),
-                                 sr.ExploreAcclerateRwd(weight=0.05)
+                                 sr.ExploreAcclerateRwd(weight=0.1)
                                 ]
 
-        self._backward_rewards = [sr.HomeReward(back=True, negative=False,weight=0.05),
+        self._backward_rewards = [sr.HomeReward(back=True, negative=True,weight=0.1),
                                   #sr.EnemyBaseReward(back=True, negative=False,weight=1),
                                   sr.HomeArrivedReward(weight=1),
                                   #sr.MinDistReward(negative=True),
@@ -41,8 +41,8 @@ class ExploreWithEvadeRwdWrapper(ScoutExploreEvadeRwd):
                                   # sr.HitEnemyBaseReward(weight=50),
                                   ]
 
-        self._final_rewards = [sr.RoundTripFinalReward(weight=0.5),
-                               er.EvadeUnderAttackRwd(weight=0.05)
+        self._final_rewards = [sr.RoundTripFinalReward(weight=1),
+                               er.EvadeUnderAttackRwd(weight=0.1)
                                # er.EvadeFinalRwd(weight=50)
                                ]
 
